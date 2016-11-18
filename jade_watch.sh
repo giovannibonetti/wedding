25ab85246d147267f6d174a57b7706fbc9e04dfb
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2016-11-18 01:59:34 -0200

    bootstrap theme

--- jade_watch.sh
+++ jade_watch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-find . -name *.html | xargs rm
-echo "Warning: all old HTML files at have been deleted!"
-jade -P --watch .
+# find . -name *.html | xargs rm
+# echo "Warning: all old HTML files at have been deleted!"
+jade -P --watch index.jade
